68622e0e48d186f79b0cd41b931c86805a468ad7
Lee Garam <47494005+KnightChaser@users.noreply.github.com>
2023-07-07 00:08:44 +0900

    Create indentationSolution.cpp

---
+++ cpp/indentationSolution.cpp
@@ -0,0 +1,15 @@
+// Don't fight to decide which is right between
+// tab for indentation or space for indentation
+// I'm sure that you'll fall in love with "semicolon indentation" that is very explicit and intuitive!
+
+#include <iostream>
+
+int main (int argc, char* arg[]) {
+;;;;int i = 0
+;;;;while (i < 10) {
+;;;;;;;;printf("%d\n", i)
+;;;;;;;;i++
+;;;;;;;;}
+;;;;return 0
+;;;;
+}
