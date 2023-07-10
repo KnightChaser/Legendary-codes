ad9b7685b3c0a2dd85e146452a65159e91e18323
Lee Garam <47494005+KnightChaser@users.noreply.github.com>
2023-07-11 04:14:58 +0900

    Create indentationSolution2.cpp

---
+++ cpp/indentationSolution2.cpp
@@ -0,0 +1,16 @@
+// You don't like the first indentation solution?
+// Then this is the alternative and ultimate solution. JUST. USE. {}.
+
+#include <iostream>
+
+int main (int argc, char* arg[]) {
+{}{}int i = 0
+{}{}while (i < 10) {
+{}{}{}{}printf("%d\n", i)
+{}{}{}{}i++
+{}{}{}{}}
+{}{}return 0
+{}{}
+}
+
+// Damn, it's so super cool!
