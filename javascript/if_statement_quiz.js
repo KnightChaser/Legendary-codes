1f42bd2ba56fc9bf0e3c8be5c311d8c4a2323aa6
Lee Garam <47494005+KnightChaser@users.noreply.github.com>
2023-07-12 01:24:25 +0900

    Create if_statement_quiz.js

---
+++ javascript/if_statement_quiz.js
@@ -0,0 +1,14 @@
+// Love coding puzzles?
+// Guess the outputs!
+
+function speak(arg) {
+	if (arg = "Meow!") {
+		console.log("Feed me, now!");
+	} else if (arg = "Purr!") {
+		console.log("Pet me!");
+	}
+}
+
+speak("Meow!")    // Feed me, now!
+speak("Purr!")    // Feed me, now!
+speak("Hiss!")    // Feed me, now!
