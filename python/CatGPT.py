41e52a6cd5e1455b36dec6f3938f4b3e88adefa2
Lee Garam <47494005+KnightChaser@users.noreply.github.com>
2023-08-13 22:58:37 +0900

    Create CatGPT.py

---
+++ python/CatGPT.py
@@ -0,0 +1,7 @@
+# Ultimate GPT model implementation for cats
+
+import random as rnd
+
+def CatGPT(input):
+	responses = ['miau', 'miauuu', 'miiiau', 'miiiiiauuu', 'meeeooow', 'meow']
+	return rnd.choice(responses)
