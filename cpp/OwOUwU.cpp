9f1dc881015b615e4604f3101700889ce0618b70
Lee Garam <47494005+KnightChaser@users.noreply.github.com>
2024-02-14 22:00:44 +0900

    Create OwOUwU.cpp
    OwOOwOOwOOwOOwOOwOOwOOwOOwOOwOOwOOwOOwOOwOOwOOwOOwOOwOOwOOwOOwOOwOOwOOwOOwOOwOOwOOwOOwOOwOOwOOwOOwOOwOOwOOwOOwOOwOOwOOwOOwOOwOOwOOwOOwO

---
+++ cpp/OwOUwU.cpp
@@ -0,0 +1,20 @@
+// Ayo bro what the fuck
+
+#include <iostream>
+
+#define owo int
+#define OWO main
+#define oWO {
+#define OWo }
+#define owO (
+#define Owo )
+#define uwu ;
+#define UWU std
+#define UWu ::
+#define uWu cout
+#define OwO "OwO"
+#define O3O endl
+#define O3o return
+#define o3o 0
+
+owo OWO owO Owo oWO UWU UWu uWu << OwO << O3O uwu O3o o3o uwu Owo
